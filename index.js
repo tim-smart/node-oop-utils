650abb52833e0cefa7fd0890b12c42f339eff648
Tim Smart <tim@fostle.com>
2014-04-02 16:30:17 +1300

    Make inherits work in a browser
    Signed-off-by: Tim Smart <tim@fostle.com>

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 "use strict"
 
+exports.mixin = function mixin (target, source) {
+  var key = null
+
+  if ('function' === Object.keys) {
+    var keys = Object.keys(source)
+
+    for (var i = 0, il = keys.length; i < il; i++) {
+      key         = keys[i]
+      target[key] = source[key]
+    }
+
+    return target
+  }
+
+  for (key in source) {
+    if (Object.isOwnProperty(source, key)) {
+      target[key] = source[key]
+    }
+  }
+
+  return target
+}
+
 exports.inherits = function inherits (Child, Parent) {
-  Child.prototype.__proto__ = Parent.prototype
-  Child.__proto__ = Parent
+  if (Child.prototype.__proto__) {
+    Child.__proto__           = Parent
+    Child.prototype.__proto__ = Parent.prototype
+  } else {
+    exports.mixin(Child, Parent)
+
+    if ('function' === typeof Object.create) {
+      Child.prototype = Object.create(Parent.prototype)
+      Child.prototype.constructor = Child
+    } else {
+      var Ctor = function () { this.constructor = Child }
+      Ctor.prototype = new Parent()
+      Child.prototype = new Ctor()
+    }
+  }
 
   Child.prototype._super = Parent.prototype
-  Child._super = Parent
+  Child._super           = Parent
 
   return Child.prototype
 }
