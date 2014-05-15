c1aa1c4e23c9a0df041abf4efc4c989ef61054aa
Tim Smart <tim.smart@designworks.co.nz>
2014-05-15 14:26:22 +1200

    Update style and fix errors
    Signed-off-by: Tim Smart <tim.smart@designworks.co.nz>

--- index.js
+++ index.js
@@ -1,47 +1,50 @@
-"use strict"
+'use strict';
 
-exports.mixin = function mixin (target, source) {
-  var key = null
+exports.mixin = function mixin(target, source) {
+    var key = null;
 
-  if ('function' === Object.keys) {
-    var keys = Object.keys(source)
+    if ('function' === Object.keys) {
+        var keys = Object.keys(source);
 
-    for (var i = 0, il = keys.length; i < il; i++) {
-      key         = keys[i]
-      target[key] = source[key]
+        for (var i = 0, il = keys.length; i < il; i += 1) {
+            key         = keys[i];
+            target[key] = source[key];
+        }
+
+        return target;
     }
 
-    return target
-  }
-
-  for (key in source) {
-    if (Object.isOwnProperty(source, key)) {
-      target[key] = source[key]
+    for (key in source) {
+        if (Object.hasOwnProperty(source, key)) {
+            target[key] = source[key];
+        }
     }
-  }
 
-  return target
-}
+    return target;
+};
 
-exports.inherits = function inherits (Child, Parent) {
-  if (Child.prototype.__proto__) {
-    Child.__proto__           = Parent
-    Child.prototype.__proto__ = Parent.prototype
-  } else {
-    exports.mixin(Child, Parent)
-
-    if ('function' === typeof Object.create) {
-      Child.prototype = Object.create(Parent.prototype)
-      Child.prototype.constructor = Child
+exports.inherits = function inherits(Child, Parent) {
+    /* jshint proto: true */
+    if (Child.prototype.__proto__) {
+        Child.__proto__           = Parent;
+        Child.prototype.__proto__ = Parent.prototype;
     } else {
-      var Ctor = function () { this.constructor = Child }
-      Ctor.prototype = new Parent()
-      Child.prototype = new Ctor()
+        exports.mixin(Child, Parent);
+
+        if ('function' === typeof Object.create) {
+            Child.prototype = Object.create(Parent.prototype);
+            Child.prototype.constructor = Child;
+        } else {
+            var Ctor = function() {
+                this.constructor = Child;
+            };
+            Ctor.prototype = new Parent();
+            Child.prototype = new Ctor();
+        }
     }
-  }
 
-  Child.prototype._super = Parent.prototype
-  Child._super           = Parent
+    Child.prototype._super = Parent.prototype;
+    Child._super           = Parent;
 
-  return Child.prototype
-}
+    return Child.prototype;
+};
